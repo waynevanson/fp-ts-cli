ac733e33d92b8e72e5fa38dc41202e955d8d4dd7
Wayne Van Son <waynevanson@gmail.com>
2022-10-18 09:40:57 +1100

    tests(command): move `flag__` to `flag` and enable

--- src/parsers/command.spec.ts
+++ src/parsers/command.spec.ts
@@ -6,67 +6,73 @@ import { kebabCase, toBuffer } from "../test-utils"
 import { option } from "fp-ts"
 import fc from "fast-check"
 
-describe("flag new", () => {
-  it("should allow a single flag", () => {
-    const flag_ = fc.tuple(kebabCase, fc.string()).map(([long, argument]) => ({
-      long,
-      argument,
-      flag: pipe(flag.long(long), flag.argumentless(argument), flag.required),
-    }))
-
-    fc.assert(
-      fc.property(flag_, fc.string(), ({ argument, flag, long }, name) => {
-        const buffer = toBuffer([`--${long}`])
-        const start = stream.stream(buffer)
-        const next = stream.stream(buffer, buffer.length)
-        const parser = command.flags__({ [name]: flag })
-        const result = parser(start)
-        const value = tuple({ [name]: argument }, constVoid())
-        const expected = parseResult.success(value, next, start)
-        expect(result).toStrictEqual(expected)
-      })
-    )
-  })
-
-  it("should allow two flags in conjunction", () => {
-    const flag_ = fc
-      .tuple(kebabCase, fc.string(), fc.string())
-      .map(([long, argument, name]) => ({
-        long,
-        argument,
-        name,
-        flag: pipe(flag.long(long), flag.argumentless(argument), flag.required),
-      }))
-
-    const pairs = fc
-      .tuple(flag_, flag_)
-      .filter(([one, two]) => one.name !== two.name)
-      .filter(([one, two]) => one.long !== two.long)
-
-    fc.assert(
-      fc.property(pairs, ([one, two]) => {
-        const buffer = toBuffer([`--${one.long}`, `--${two.long}`])
-        const start = stream.stream(buffer)
-        const next = stream.stream(buffer, buffer.length)
-        const parser = command.flags__({
-          [one.name]: one.flag,
-          [two.name]: two.flag,
-        })
-        const result = parser(start)
-        const value = tuple(
-          { [one.name]: one.argument, [two.name]: two.argument },
-          constVoid()
-        )
-        const expected = parseResult.success(value, next, start)
-        expect(result).toStrictEqual(expected)
-      })
-    )
-  })
-})
-
-// flags -> arguments ->
-describe.skip("command", () => {
+describe("command", () => {
   describe("flags", () => {
+    it("should allow a single flag", () => {
+      const flag_ = fc
+        .tuple(kebabCase, fc.string())
+        .map(([long, argument]) => ({
+          long,
+          argument,
+          flag: pipe(
+            flag.long(long),
+            flag.argumentless(argument),
+            flag.required
+          ),
+        }))
+
+      fc.assert(
+        fc.property(flag_, fc.string(), ({ argument, flag, long }, name) => {
+          const buffer = toBuffer([`--${long}`])
+          const start = stream.stream(buffer)
+          const next = stream.stream(buffer, buffer.length)
+          const parser = command.flags({ [name]: flag })
+          const result = parser(start)
+          const value = tuple({ [name]: argument }, constVoid())
+          const expected = parseResult.success(value, next, start)
+          expect(result).toStrictEqual(expected)
+        })
+      )
+    })
+
+    it("should allow two flags in conjunction", () => {
+      const flag_ = fc
+        .tuple(kebabCase, fc.string(), fc.string())
+        .map(([long, argument, name]) => ({
+          long,
+          argument,
+          name,
+          flag: pipe(
+            flag.long(long),
+            flag.argumentless(argument),
+            flag.required
+          ),
+        }))
+
+      const pairs = fc
+        .tuple(flag_, flag_)
+        .filter(([one, two]) => one.name !== two.name)
+        .filter(([one, two]) => one.long !== two.long)
+
+      fc.assert(
+        fc.property(pairs, ([one, two]) => {
+          const buffer = toBuffer([`--${one.long}`, `--${two.long}`])
+          const start = stream.stream(buffer)
+          const next = stream.stream(buffer, buffer.length)
+          const parser = command.flags({
+            [one.name]: one.flag,
+            [two.name]: two.flag,
+          })
+          const result = parser(start)
+          const value = tuple(
+            { [one.name]: one.argument, [two.name]: two.argument },
+            constVoid()
+          )
+          const expected = parseResult.success(value, next, start)
+          expect(result).toStrictEqual(expected)
+        })
+      )
+    })
     it("should ensure both required flags are present", () => {
       expect.assertions(1)
 
