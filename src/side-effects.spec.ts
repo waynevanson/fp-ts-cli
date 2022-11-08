c9b05ddd617061e8469cff2501a5ad96caa54d48
Wayne Van Son <waynevanson@gmail.com>
2022-11-08 19:41:36 +1100

    style: remove swearing in comments

--- src/side-effects.spec.ts
+++ src/side-effects.spec.ts
@@ -1,7 +1,6 @@
 import { argvNode } from "./side-effects"
 import process from "process"
 
-// fuckin mocks jesus christ
 describe("argvNode", () => {
   it("should take the value from process.argv", () => {
     const argv_ = ["1", "2", "3"]
