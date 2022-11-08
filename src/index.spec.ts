0a25212590b1015759cd7530dfd64a8fade73273
Wayne Van Son <waynevanson@gmail.com>
2022-11-08 19:28:46 +1100

    feat: add `Input` for node

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,32 @@
-import { run } from "./index"
+import { option, readonlyArray } from "fp-ts"
+import { pipe } from "fp-ts/lib/function"
+import { run, node, Input } from "./index"
+import * as sideEffects from "./side-effects"
 
 describe("cli", () => {
   it("should parse the input?", () => {
     const data = ["runtime", "file"]
     const result = run(data)
     expect(result).toStrictEqual([])
+  })
+
+  it("should get the node environment from the outside world", () => {
+    const args: ReadonlyArray<string> = []
+    const runtime = "runtime"
+    const file = "file"
+
+    const argv = pipe([runtime, file], readonlyArray.concat(args))
+
+    jest.spyOn(sideEffects, "argvNode").mockImplementation(() => argv)
+
+    const result = node()
+
+    const expected: Input = {
+      runtime: option.some(runtime),
+      file: option.some(file),
+      args,
+    }
+
+    expect(result).toStrictEqual(expected)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { pipe } from "fp-ts/lib/function"
-import { readonlyArray } from "./fp"
+import { io, option, readonlyArray } from "./fp"
+import { argvNode } from "./side-effects"
 
 export type Arg = string
 
 export type Argv = ReadonlyArray<Arg>
 
 export function run(argv: Argv): Argv {
-  return pipe(
-    argv,
-    readonlyArray.filterWithIndex((index) => index >= 2)
-  )
+  return pipe(argv, readonlyArray.dropLeft(2))
 }
+
+export interface Input {
+  readonly runtime: option.Option<string>
+  readonly file: option.Option<string>
+  readonly args: ReadonlyArray<string>
+}
+
+export const node: io.IO<Input> = pipe(
+  io.Do,
+  io.bind("runtime", () => pipe(argvNode, io.map(readonlyArray.lookup(0)))),
+  io.bind("file", () => pipe(argvNode, io.map(readonlyArray.lookup(1)))),
+  io.bind("args", () => pipe(argvNode, io.map(readonlyArray.dropLeft(2))))
+)
