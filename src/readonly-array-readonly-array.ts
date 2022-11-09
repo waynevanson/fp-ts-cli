990fed0c00322d1ba880f28389faa23479777932
Wayne Van Son <waynevanson@gmail.com>
2022-11-10 08:04:43 +1100

    feat: add double array and parserargs

---
+++ src/readonly-array-readonly-array.ts
@@ -0,0 +1,9 @@
+export const URI = "ReadonlyArrayReadonlyArray"
+export type URI = typeof URI
+export type ReadonlyArrayReadonlyArray<A> = ReadonlyArray<ReadonlyArray<A>>
+
+declare module "fp-ts/HKT" {
+  export interface URItoKind<A> {
+    readonly [URI]: ReadonlyArrayReadonlyArray<A>
+  }
+}
