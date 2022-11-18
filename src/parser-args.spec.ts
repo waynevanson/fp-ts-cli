b6d836119c01ba44eafceeedbe318c50249fcf1d
Wayne Van Son <waynevanson@gmail.com>
2022-11-19 07:46:31 +1100

    test(parser-args): update and skip tests

--- src/parser-args.spec.ts
+++ src/parser-args.spec.ts
@@ -3,34 +3,18 @@ import * as parseResultWithIndex from "./parse-result-with-index"
 import * as parserArgs from "./parser-args"
 
 describe("parser-args", () => {
-  it("should allow parsing up to a whole", () => {
-    const name = "flag-name"
-    const nameExtended = `${name}-extended`
-    const inputArg = `--${nameExtended}`
-    const input = [inputArg, "next-f"]
-    const buffer = toBuffer(input)
+  describe("outer", () => {
+    it.skip("should allow parsing up to a whole", () => {
+      const outer = "flag-name"
+      const input = [outer, "next-f"]
+      const buffer = toBuffer(input)
 
-    const start = parseResultWithIndex.stream(buffer, { outer: 0, inner: 0 })
-    const next = parseResultWithIndex.stream(buffer, { outer: 0, inner: 11 })
-    const expected = parseResultWithIndex.success(name, next, start)
+      const start = parseResultWithIndex.stream(buffer, { outer: 0, inner: 0 })
+      const next = parseResultWithIndex.stream(buffer, { outer: 0, inner: 11 })
+      const expected = parseResultWithIndex.success(outer, next, start)
 
-    // disjunction is either,
-    // conjunction is both.
-  })
-
-  // with two next buffers, which do we choose?
-  // two successes?
-  // if one, which arg to use?
-  describe("both", () => {
-    const one = "flag-name"
-    const two = `${one}-extended`
-    const input = [two]
-    const buffer = toBuffer(input)
-
-    const start = parseResultWithIndex.stream(buffer, { outer: 0, inner: 0 })
-    const next = parseResultWithIndex.stream(buffer, { outer: 0, inner: 11 })
-    const expected = parseResultWithIndex.success(one, next, start)
-
-    // const parserOne = parserArgs.takeUntilWithIndex()
+      const result = parserArgs.outer(start)
+      expect(result).toStrictEqual(expected)
+    })
   })
 })
