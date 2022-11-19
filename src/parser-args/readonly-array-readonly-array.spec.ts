62fc349383cde7f1de6d2f0e1d7d29cddaed23ff
Wayne Van Son <waynevanson@gmail.com>
2022-11-19 11:57:18 +1100

    test(rara): add `Indexable.lookup` fast-check test

--- src/parser-args/readonly-array-readonly-array.spec.ts
+++ src/parser-args/readonly-array-readonly-array.spec.ts
@@ -1,3 +1,4 @@
+import fc from "fast-check";
 import { option } from "fp-ts";
 import * as rara from "./readonly-array-readonly-array";
 
@@ -20,6 +21,64 @@ describe("ReadonlyArrayReadonlyArray", () => {
         const result = rara.Indexable.lookup(index)(data);
         expect(result).toStrictEqual(option.of("four"));
       });
+
+      it.concurrent("should look up at the index lots", () => {
+        const fromnonemptyarrays = <A>(arrays: Array<Array<A>>) =>
+          fc.integer({ min: 0, max: arrays.length - 1 }).chain((outer) =>
+            fc
+              .integer({ min: 0, max: arrays[outer].length - 1 })
+              .map((inner) => ({
+                inner,
+                outer,
+                arrays,
+                indexed: arrays[outer][inner],
+              }))
+          );
+
+        const arrays = fc.array(fc.array(fc.string(), { minLength: 1 }), {
+          minLength: 1,
+        });
+
+        const arbitrary = arrays.chain(fromnonemptyarrays);
+
+        fc.assert(
+          fc.property(arbitrary, ({ arrays, inner, outer, indexed }) => {
+            const index = { outer, inner };
+            const result = rara.Indexable.lookup(index)(arrays);
+            expect(result).toStrictEqual(option.of(indexed));
+          })
+        );
+      });
     });
+
+    it.concurrent(
+      "should look up and fail if any index is out of range",
+      () => {
+        const fromnonemptyarrays = <A>(arrays: Array<Array<A>>) =>
+          fc.integer().chain((outer) =>
+            fc
+              .integer()
+              .filter(
+                (inner) =>
+                  arrays[outer]?.length == null ||
+                  inner < 0 ||
+                  inner > arrays[outer].length
+              )
+              .map((inner) => ({ inner, outer, arrays }))
+          );
+
+        const arrays = fc.array(fc.array(fc.string()));
+
+        const arbitrary = arrays.chain(fromnonemptyarrays);
+
+        fc.assert(
+          fc.property(arbitrary, ({ arrays, inner, outer }) => {
+            const index = { outer, inner };
+            const result = rara.Indexable.lookup(index)(arrays);
+            expect(result).toStrictEqual(option.none);
+          })
+        );
+      }
+    );
   });
 });
