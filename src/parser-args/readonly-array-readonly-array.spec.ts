5e0aec1319e2d00ef21d36726b58d5052f77623b
Wayne Van Son <waynevanson@gmail.com>
2022-11-19 11:35:32 +1100

    test(rara): add test for `Indexable.lookup

--- src/parser-args/readonly-array-readonly-array.spec.ts
+++ src/parser-args/readonly-array-readonly-array.spec.ts
@@ -1,8 +1,25 @@
+import { option } from "fp-ts";
 import * as rara from "./readonly-array-readonly-array";
 
-describe("URI", () => {
-  it.concurrent("should be ReadonlyArrayReadonlyArray", () => {
-    const uri = "ReadonlyArrayReadonlyArray";
-    expect(rara.URI).toBe(uri);
+describe("ReadonlyArrayReadonlyArray", () => {
+  describe("URI", () => {
+    it.concurrent("should be ReadonlyArrayReadonlyArray", () => {
+      const uri = "ReadonlyArrayReadonlyArray";
+      expect(rara.URI).toBe(uri);
+    });
+  });
+
+  describe("Indexable", () => {
+    describe("lookup", () => {
+      it.concurrent("should look up at the index", () => {
+        const index = { outer: 1, inner: 1 };
+        const data = [
+          ["one", "two"],
+          ["three", "four"],
+        ];
+        const result = rara.Indexable.lookup(index)(data);
+        expect(result).toStrictEqual(option.of("four"));
+      });
+    });
   });
 });
