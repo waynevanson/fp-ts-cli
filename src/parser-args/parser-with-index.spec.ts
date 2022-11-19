3fb7797ee20258731832be9b22b772de67c2b14a
Wayne Van Son <waynevanson@gmail.com>
2022-11-19 14:22:29 +1100

    tests(parser-with-index): add test for `map`

--- src/parser-args/parser-with-index.spec.ts
+++ src/parser-args/parser-with-index.spec.ts
@@ -1,6 +1,7 @@
 import * as parserWithIndex from "./parser-with-index";
 import * as streamWithIndex from "./stream-with-index";
 import * as parseResultWithIndex from "./parse-result-with-index";
+import { pipe } from "fp-ts/lib/function";
 
 describe("parserWithIndex", () => {
   describe("of", () => {
@@ -10,6 +11,23 @@ describe("parserWithIndex", () => {
       const start = streamWithIndex.stream(buffer, "three");
       const result = parserWithIndex.of(value)(start);
       const expected = parseResultWithIndex.success(value, start, start);
+
+      expect(result).toStrictEqual(expected);
+    });
+  });
+
+  describe("map", () => {
+    it("should apply a function on the parsers value", () => {
+      const value = "a";
+      const buffer = ["one", "two"];
+      const start = streamWithIndex.stream(buffer, "three");
+      const f = (a: string) => a + "b";
+      const result = pipe(
+        parserWithIndex.of(value),
+        parserWithIndex.map(f)
+      )(start);
+
+      const expected = parseResultWithIndex.success(f(value), start, start);
 
       expect(result).toStrictEqual(expected);
     });
